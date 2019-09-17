7e54ec9eab616d80aa0828476211c20a5914599d
Ben Mandakh <benmandakh@gmail.com>
2019-09-17 11:03:11 -0700

    Angular DOM

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
+import { importType } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ben';
+export class AppComponent implements OnInit {
+  // title = 'ben';
+  tasks = [];
+
   constructor(private _httpService: HttpService){}
+  ngOnInit(){
+    this.getTasksFromService()
+  }
+  getTasksFromService(){
+    let observable = this._httpService.getTasks()
+    observable.subscribe(data => {
+      console.log("Got our tasks!", data)
+      this.tasks = data;
+    }) 
+  }
+  /////////////////
+  // snacks: string[];
+  // loggedIn: boolean;
+  // /////////////////
+  // num: number;
+  // randNum: number;
+  // str: string;
+  // first_name: string;
+
+  // ngOnInit(){
+  //   ///////////////
+  //   this.snacks = ["vanella latte with skim milk", "brushed suede", "cookie", "orio"];
+  //   this.loggedIn = true;
+  //   //////////////
+  //   this.num = 7;
+  //   this.randNum = Math.floor((Math.random() * 2) + 1);
+  //   this.str = " Hello Angular Visitor! ";
+  //   this.first_name = "Alpha";
+  // }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -5,17 +5,18 @@ import { HttpClient } from '@angular/common/http';
 
 export class HttpService {
   constructor(private _http:HttpClient) {
-    this.getTasks();
-    this.getPokemon();
+    // this.getTasks();
+    // this.getPokemon();
    }
   
 
   getTasks(){
-    let tempObservable = this._http.get('/tasks');
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // let tempObservable = this._http.get('/tasks');
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/tasks');
   }
-  getPokemon(){
-    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
-    bulbasaur.subscribe(data => console.log("Got our Pokemon information!", data));
-  }
+  // getPokemon(){
+  //   let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+  //   bulbasaur.subscribe(data => console.log("Got our Pokemon information!", data));
+  // }
 }
