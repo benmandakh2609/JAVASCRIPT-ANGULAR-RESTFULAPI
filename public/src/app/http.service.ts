9ec446d2b0e1273ff8ea8d2dd4c0e38411cf1fc7
Ben Mandakh <benmandakh@gmail.com>
2019-09-16 18:27:18 -0700

    Added pokemon api

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -6,11 +6,16 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
   constructor(private _http:HttpClient) {
     this.getTasks();
+    this.getPokemon();
    }
   
 
   getTasks(){
     let tempObservable = this._http.get('/tasks');
     tempObservable.subscribe(data => console.log("Got our tasks!", data));
+  }
+  getPokemon(){
+    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+    bulbasaur.subscribe(data => console.log("Got our Pokemon information!", data));
   }
 }
